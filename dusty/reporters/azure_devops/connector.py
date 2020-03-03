97192394b4d97d7e4520669dd1a1bfdc80fd6ed1
Artem Rozumenko <artyom.rozumenko@gmail.com>
2020-03-03 09:50:11 +0200

    cleaned code

--- dusty/reporters/azure_devops/connector.py
+++ dusty/reporters/azure_devops/connector.py
@@ -10,40 +10,25 @@ class ADOConnector(object):
             self.team = f"{project}\\{team}"
         self.url = c.CREATE_ISSUE_URL.format(organization=organization, project=project,
                                              type=issue_type, rules="false", notify="false")
-        self.query_url = "https://dev.azure.com/{organization}/{project}/{team}/" \
-                         "_apis/wit/wiql?api-version=5.1".format(organization=organization, team=team,
-                                                                 project=project)
+        self.query_url = c.QUERY_ISSUE_URL.format(organization=organization, project=project)
 
     def create_finding(self, title, description=None, priority=None,
                        assignee=None, issue_hash=None, custom_fields=None):
         if not custom_fields:
             custom_fields = dict()
         body = []
-        if title:
-            title_piece = c.TITLE
-            title_piece['value'] = title
-            body.append(title_piece)
-        if description:
-            description_piece = c.DESCRIPTION
-            description_piece['value'] = description
-            body.append(description_piece)
-        if priority:
-            priority_piece = c.PRIORITY
-            priority_piece['value'] = c.PRIORITY_MAPPING[priority]
-            body.append(priority_piece)
-        if assignee:
-            assignee_piece = c.ASSIGNED_TO
-            assignee_piece['value'] = assignee
-            body.append(assignee_piece)
-        team_piece = c.TEAM
-        team_piece['value'] = self.team
-        body.append(team_piece)
-        iteration_piece = c.ITERATION
-        iteration_piece['value'] = self.team
-        body.append(iteration_piece)
-        for each in custom_fields:
-            _piece = {"op": "add", "path": each, "from": None, "value": custom_fields[each]}
-            body.append(_piece)
+        fields_mapping = {
+            "/fields/System.Title": title,
+            "/fields/Microsoft.VSTS.Common.Priority": priority,
+            "/fields/System.Description": description,
+            "/fields/System.AssignedTo": assignee,
+            "/fields/System.AreaPath": self.team,
+            "/fields/System.IterationPath": self.team
+        }
+        for key, value in {**fields_mapping, **custom_fields}.items():
+            if value:
+                _piece = {"op": "add", "path": key, "from": None, "value": value}
+                body.append(_piece)
         if not self.search_for_issue(issue_hash):
             return post(self.url, auth=self.auth, json=body,
                         headers={'content-type': 'application/json-patch+json'}).content
