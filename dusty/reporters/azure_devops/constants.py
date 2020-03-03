40afabd588874c8b0af362b14892e64b99460885
Artem Rozumenko <artyom.rozumenko@gmail.com>
2020-03-03 09:50:47 +0200

    cleaned code

--- dusty/reporters/azure_devops/constants.py
+++ dusty/reporters/azure_devops/constants.py
@@ -21,11 +21,6 @@
 
 PRIORITY_MAPPING = {"Critical": 1, "High": 1, "Medium": 2, "Low": 3, "Info": 4}
 
-TITLE = {"op": "add", "path": "/fields/System.Title", "from": None, "value": ""}
-PRIORITY = {"op": "add", "path": "/fields/Microsoft.VSTS.Common.Priority", "from": None, "value": 2}
-DESCRIPTION = {"op": "add", "path": "/fields/System.Description", "from": None, "value": ""}
-ASSIGNED_TO = {"op": "add", "path": "/fields/System.AssignedTo", "from": None, "value": ""}
-TEAM = {"op": "add", "path": "/fields/System.AreaPath", "from": None, "value": ""}
-ITERATION = {"op": "add", "path": "/fields/System.IterationPath", "from": None, "value": ""}
 CREATE_ISSUE_URL = 'https://dev.azure.com/{organization}/{project}/_apis/wit/workitems/' \
                    '${type}?bypassRules={rules}&suppressNotifications={notify}&api-version=5.1'
+QUERY_ISSUE_URL = "https://dev.azure.com/{organization}/{project}/_apis/wit/wiql?api-version=5.1"
